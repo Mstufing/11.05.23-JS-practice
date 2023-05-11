b765f05b87d9fcb69c5acdf5ab451ed86e6fc4b8
Mstufing <mstufingms11@gmail.com>
2023-05-11 11:02:11 +0300

    task #11

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 
-
 // ? Task #1
 // ? Add the Index
 
-function addIndexes(arr) {
-    let myArr = [];
-    myArr = arr.map((elem, index) => elem + index);
-    return myArr;
-}
+// function addIndexes(arr) {
+//     let myArr = [];
+//     myArr = arr.map((elem, index) => Number(elem) + Number(index));
+//     return myArr;
+// }
+
+// let myBtn = document.querySelector(".btn");
+
+// myBtn.onclick = () => {
+//     let myInp1 = document.querySelector(".inp1");
+//     let myResult = document.querySelector(".result");
+
+//     let myArr = myInp1.value.trim().split(" ");
+//     myResult.innerHTML = addIndexes(myArr);
+// }
