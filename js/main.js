046cbfd18966ce24351bde83420a50b084e12918
Mstufing <mstufingms11@gmail.com>
2023-05-11 17:13:01 +0300

    task #2 and #3

--- js/main.js
+++ js/main.js
@@ -17,3 +17,42 @@
 //     let myArr = myInp1.value.trim().split(" ");
 //     myResult.innerHTML = addIndexes(myArr);
 // }
+
+
+// ? Task #2
+// ? Filter Strings from Array
+// ! Function works but DOM not ? What's wrong ? Check it !
+
+// function filterArray(arr) {
+//     return arr.filter(elem => typeof elem == "number" && Number.isInteger(elem));
+// }
+
+// let myBtn = document.querySelector(".btn");
+
+// myBtn.onclick = () => {
+//     let myInp1 = document.querySelector(".inp1");
+//     let myResult = document.querySelector(".result");
+
+//     let myArr = myInp1.value.trim().split(" ");
+//     myResult.innerHTML = filterArray(myArr);
+// }
+
+
+// ? Task #3
+// ? True Ones, False Zeros
+
+// function integerBoolean(n) {
+//     let arr = [...n];
+//     return arr.map(elem => elem == 1 ? true : false);
+// }
+
+// let myBtn = document.querySelector(".btn");
+
+// myBtn.onclick = () => {
+//     let myInp1 = document.querySelector(".inp1");
+//     let myResult = document.querySelector(".result");
+
+//     let myArr = myInp1.value.trim().split(" ");
+//     myResult.innerHTML = integerBoolean(myArr);
+//     console.log(myArr)
+// }
