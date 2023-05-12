3c895b7ae439b23d7d693d89f63d82eb9a17e281
Mstufing <mstufingms11@gmail.com>
2023-05-12 06:23:08 +0300

    tasks #7, 8, 9, 10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+// ? -------Array-------
 // ? Task #1
 // ? Add the Index
 
@@ -121,4 +122,90 @@
 //     let myArray = inp1.value.trim().split(" ");
 
 //     result.innerHTML = cleanUpArray(myArray);
+// }
+
+// ? Task #7
+// ? Age Difference Between Spouses
+
+// function ageDifference(ages) {
+//     ages.sort((a, b) => b - a);
+
+//     if (ages[0] === ages[1])
+//         return "No age difference between spouses.";
+//     else if (ages[0] - ages[1] === 1)
+//         return "1 year";
+//     else
+//         return `${ages[0] - ages[1]} years`
+// }
+
+// let btn = document.querySelector(".btn");
+
+// btn.onclick = () => {
+//     let inp1 = document.querySelector(".inp1");
+//     let result = document.querySelector(".result");
+
+//     let myArray = inp1.value.trim().split(" ");
+
+//     result.innerHTML = ageDifference(myArray);
+// }
+
+
+// ? -------String-------
+
+// ? Task #8
+// ? City School Creating IDS
+// ! Try to solve in another way !
+
+// function createID(firstname, lastname) {
+//     return firstname.charAt(0).toLowerCase() + lastname.charAt(0).toUpperCase() + lastname.charAt(1).toLowerCase() + lastname.charAt(2).toLowerCase();
+// }
+
+// let btn = document.querySelector(".btn");
+
+// btn.onclick = () => {
+//     let inp1 = document.querySelector(".inp1");
+//     let inp2 = document.querySelector(".inp2");
+//     let result = document.querySelector(".result");
+
+//     result.innerHTML = createID(inp1.value, inp2.value);
+// }
+
+
+// ? Task #9
+// ? Modifying the Last Character
+
+
+// function modifyLast(str, n) {
+//     let lastChar = str.charAt(str.length - 1);
+//     return str.concat(lastChar.repeat(n - 1));
+// }
+
+// let btn = document.querySelector(".btn");
+
+// btn.onclick = () => {
+//     let inp1 = document.querySelector(".inp1");
+//     let inp2 = document.querySelector(".inp2");
+//     let result = document.querySelector(".result");
+
+//     result.innerHTML = modifyLast(inp1.value, inp2.value);
+// }
+
+
+// ? Task #10
+// ? Height of an Equilateral Triangle
+// ! Try to get how round() and toFixed() methods worked in this task !!
+// function height(side) {
+//     let a = Math.sqrt(side * side - ((side / 2) * (side / 2)));
+//     let b = (Math.round(a * 100) / 100).toFixed(2);
+//     return `${b * 10} mm`
+// }
+
+
+// let btn = document.querySelector(".btn");
+
+// btn.onclick = () => {
+//     let inp1 = document.querySelector(".inp1");
+//     let result = document.querySelector(".result");
+
+//     result.innerHTML = height(inp1.value);
 // }
